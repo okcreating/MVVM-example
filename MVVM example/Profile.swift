4cd0af7c99ee5ceb429dc169b29b3c7b8e27884c
okcreating <oksibearus@gmail.com>
2025-01-21 15:12:11 +0000

    MVC base is ready

---
+++ MVVM example/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  MVVM example
+//
+//  Created by Oksana Kazarinova on 21/01/2025.
+//
+
+import Foundation
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
--- MVVM example/ViewController.swift
+++ MVVM example/ViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var secondNameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+
+    var profile: Profile? {
+        didSet {
+            guard let profile = profile else { return }
+            nameLabel.text = profile.name
+            secondNameLabel.text = profile.secondName
+            ageLabel.text = "\(profile.age)"
+        }
+    }
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        profile = Profile(name: "John", secondName: "Bean", age: 27)
     }
 
 
