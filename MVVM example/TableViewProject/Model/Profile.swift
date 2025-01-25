fbb730736e816f5964114b43fe6b5cfd1aebc96f
okcreating <oksibearus@gmail.com>
2025-01-25 23:05:28 +0000

    TableView MVC base is ready

---
+++ MVVM example/TableViewProject/Model/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  MVVM example
+//
+//  Created by Oksana Kazarinova on 23/01/2025.
+//
+
+import Foundation
+
+struct TableProfile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
---
+++ MVVM example/TableViewProject/View/TableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewCell.swift
+//  MVVM example
+//
+//  Created by Oksana Kazarinova on 25/01/2025.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var age: UILabel!
+
+}
